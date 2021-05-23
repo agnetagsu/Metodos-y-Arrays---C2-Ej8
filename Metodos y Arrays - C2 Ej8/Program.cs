01779b2b892fd033f2e2db0e9f3c94e0731541ca
Agneta <agnetagsu@yahoo.es>
2021-05-23 11:58:17 +0200

    Agregar archivos de proyecto.

---
+++ Metodos y Arrays - C2 Ej8/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Metodos_y_Arrays___C2_Ej8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] miarray = new int[50];
+            
+            for (int i=0;i<miarray.Length;i++)
+            {
+                miarray[i] = i;
+                Console.WriteLine(miarray[i]);
+            }
+
+            
+        }
+    }
+}
